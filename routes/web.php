1e3a5348640a5a294bfdc40e78c6a4d6a5a16ca3
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2025-12-13 15:23:02 -0600

    feat: Autentificación por google

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +18,25 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->user();
+    $user = User::updateOrCreate(
+        [
+            'google_id' => $user_google->id,
+        ],
+        [
+            'name' => $user_google->name,
+            'email' => $user_google->email,
+        ],
+    );
+    Auth::login($user);
+    return redirect('/dashboard');
 });
 
 require __DIR__.'/auth.php';
